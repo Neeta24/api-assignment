a9727944535e8b46dfeeb17dbbe070a468d5a7b3
Neeta24 <neeta.sinha.24@gmail.com>
2022-09-04 09:25:04 +0600

    added

--- news.js
+++ news.js
@@ -1,1 +1,49 @@
-console.log('hello');
+const loadAllMenus = async()=>{
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    return data;
+
+}
+
+  const setMenus = async()=>{
+    const data =await loadAllMenus();
+    displayCategories(data.data.news_category);
+  
+  }
+
+const displayCategories = category =>{
+    const ulContainer = document.getElementById("ul-container");
+    category ?.forEach(categoryName => {
+        console.log(categoryName.category_name);
+        const li= document.createElement('li');
+    
+        li.innerHTML=`
+       <a>${(categoryName.category_name)}</a>
+        
+        `;
+        ulContainer.appendChild(li);
+    });
+
+}
+
+
+const loadAllNews = async()=>{
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories/{category_id}');
+    const data = await response.json();
+    return data;
+
+}
+
+  const setNews = async()=>{
+    const data =await loadAllNews();
+   console.log(data);
+  
+  }
+
+
+setMenus();
+displayCategories();
+
+
+
+ 
